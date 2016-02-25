438f40979da081b8c6ecdb9ba506d51353a884b4
B153 FIRE <B153 FIRE>
2016-02-25 11:07:31 -0300

    Carregamento de arquivo de empreendimentos funcionando corretamente.

--- Unity Projetos/Reciclador_Jef/Assets/Scripts/Utilidade/GerenciadorCarregamento.cs
+++ Unity Projetos/Reciclador_Jef/Assets/Scripts/Utilidade/GerenciadorCarregamento.cs
@@ -7,15 +7,34 @@ using System.Collections.Generic;
 /// </summary>
 public class GerenciadorCarregamento : MonoBehaviour
 {
+	// Tipos de fontes do arquivo de empreendimentos, interno da pasta Resources ou externo.
+	public enum TipoCarregamento {
+		InternoResources, ExternoWWW
+	}
+
+	/// <summary>	/// Quantidade do tipo de recicladoras existentes, papel, vidro, metal e plastico: 4.	/// </summary>
+	public const int quantidadeTiposRecicladoras = 4;
+
+	/// <summary>	/// Separador de linhas para o parser.	/// </summary>
 	static string [] separadorLinhas = {"\n","\n\r","\r\n","\r"};
+
+	/// <summary>	/// Divisor de tag/descriçao do item.	/// </summary>
 	static string [] divisor = {"::"};
+
+	/// <summary>	/// Caminho para o arquivo texto de empreendimentos na pasta Resources.	/// </summary>
 	static string caminhoEmpreendimentos = "empreendimentos";
 
+	// Strings usadas para reconhecimento do parser
 	static string parseEmpreendimento = "empreendimento";
 	static string parseNome = "nome";
 	static string parseDescricao = "descricao";
 	static string parseNivelMaximo = "nivelmaximo";
 	static string parseNivelEmpreendimento = "nivelempreendimento";
+	static string parseValorVenda = "valorvenda";
+	static string parseExperiencia = "experiencia";
+	static string parseVelocidadeAparecerLixo = "velocidadeaparecerlixo";
+	static string parseLimiteFila = "limitefila";
+	static string parseLixoMinimo = "lixominimo";
 	static string parseVelocidadeReciclagem = "velocidadereciclagem";
 	static string parseDinheiroGerado = "dinheirogerado";
 	static string parseSeparacaoAutomatica = "separacaoautomatica";
@@ -26,13 +45,21 @@ public class GerenciadorCarregamento : MonoBehaviour
 	static string parseFimEmpreendimento = "fimempreendimento";
 
 	/// <summary>
-	/// Carrega todos os empreendimentos de um arquivo de texto, da pasta Resources.
+	/// Carrega todos os empreendimentos de um arquivo de texto.
 	/// </summary>
 	/// <returns>Lista com os empreendimentos existentes.</returns>
-	static public Empreendimento [] CarregarEmpreendimentos()
+	static public Empreendimento [] CarregarEmpreendimentos(
+		TipoCarregamento tipo = TipoCarregamento.InternoResources)
 	{
-		string textoBruto = CarregarStringDeResources(
-			caminhoEmpreendimentos);
+		string textoBruto;
+
+		switch(tipo)
+		{
+		default:
+			textoBruto = CarregarStringDeResources(
+				caminhoEmpreendimentos);
+			break;
+		}
 
 		string [] linhas = RemoverLinhasComentarios(textoBruto);
 
@@ -59,13 +86,20 @@ public class GerenciadorCarregamento : MonoBehaviour
 				empreendimentos.Add(PegarEmpreendimento(
 					identificador, novoEmpreendimento.ToArray()));
 
+				//novoEmpreendimento.Clear();
+
 				i = j;
 			}
 			i++;
 		}
 
+		foreach(Empreendimento e in empreendimentos)
+		{
+			Debug.Log (e.ToString() + "\n");
+		}
 
-		return null;
+
+		return empreendimentos.ToArray();
 	}
 
 	/// <summary>
@@ -77,11 +111,12 @@ public class GerenciadorCarregamento : MonoBehaviour
 	static Empreendimento PegarEmpreendimento(
 		string identificador, string [] linhas)
 	{
-		//Empreendimento empreendimento = new Empreendimento();
 		string nome = "";
 		string descricao = "";
 		int nivelMaximo = 1;
 
+		// Verifica as linhas para encontrar o nome, descrição e
+		// o nível máximo do empreendimento.
 		foreach(string linha in linhas)
 		{
 			string [] linhaAtual = linha.Split(
@@ -105,7 +140,15 @@ public class GerenciadorCarregamento : MonoBehaviour
 			}
 		}
 
-		float [] velocidades = new float[nivelMaximo];
+		float [,] velocidades = new 
+			float[nivelMaximo,quantidadeTiposRecicladoras];
+		float [,] valoresVenda = new 
+			float[nivelMaximo,quantidadeTiposRecicladoras];
+		float [] experiencias = new float[nivelMaximo];
+		float [] velocidadesLixos = new float[nivelMaximo];
+		int [,] limitesRecicladoras = new
+			int[nivelMaximo,quantidadeTiposRecicladoras];
+		int [] lixosMinimos = new int[nivelMaximo];
 		int [] dinheiros = new int[nivelMaximo];
 		int [] separacoes = new int[nivelMaximo];
 		int [] poderes = new int[nivelMaximo];
@@ -114,6 +157,8 @@ public class GerenciadorCarregamento : MonoBehaviour
 		Dictionary<int,Dictionary<string, int>> construidos = 
 			new Dictionary<int,Dictionary<string, int>>();
 
+
+
 		for(int i = 0; i < nivelMaximo; i++)
 		{
 			construidos[i+1] = new Dictionary<string, int>();
@@ -121,12 +166,13 @@ public class GerenciadorCarregamento : MonoBehaviour
 
 		int nivelAtual = 1;
 
+		// Verifica a entrada para encontrar os aprimoramentos e
+		// requisitos de cada nível do empreendimento.
 		foreach(string linha in linhas)
 		{
 			string [] linhaAtual = linha.Split(
 				divisor, System.StringSplitOptions.None);
-
-						
+									
 			if (linhaAtual[0].StartsWith(parseNivelEmpreendimento))
 			{
 				int nv = 0;
@@ -134,15 +180,84 @@ public class GerenciadorCarregamento : MonoBehaviour
 				{
 					nivelAtual = nv;
 				}
-				Debug.Log ("NivelAtual = "+nivelAtual+"; NivelString: "+linhaAtual[1]+"; Linha: "+linha);
+			}
+			else if (linhaAtual[0].StartsWith(parseValorVenda))
+			{
+				linhaAtual[1] = linhaAtual[1].Replace(" ","");
+				linhaAtual[1] = linhaAtual[1].Replace("\t","");
+				
+				string [] valores = linhaAtual[1].Split(',');
+
+				for (int i = 0; i < quantidadeTiposRecicladoras; i++)
+				{
+					valoresVenda[nivelAtual-1,i] = 0;
+					float valor = 0;
+					if (valores.Length > i && float.TryParse(valores[i], out valor))
+					{
+						valoresVenda[nivelAtual-1,i] = valor / 100f;
+					}
+				}
+			}
+			else if (linhaAtual[0].StartsWith(parseExperiencia))
+			{
+				float f = 0;
+				experiencias[nivelAtual-1] = 0;
+				if (float.TryParse(linhaAtual[1], out f))
+				{
+					experiencias[nivelAtual-1] = f / 100f;
+				}
+			}
+			else if (linhaAtual[0].StartsWith(parseVelocidadeAparecerLixo))
+			{
+				float f = 0;
+				velocidadesLixos[nivelAtual-1] = 0;
+				if (float.TryParse(linhaAtual[1], out f))
+				{
+					velocidadesLixos[nivelAtual-1] = f / 100f;
+				}
+			}
+			else if (linhaAtual[0].StartsWith(parseLimiteFila))
+			{
+				linhaAtual[1] = linhaAtual[1].Replace(" ","");
+				linhaAtual[1] = linhaAtual[1].Replace("\t","");
+				
+				string [] valores = linhaAtual[1].Split(',');
+				
+				for (int i = 0; i < quantidadeTiposRecicladoras; i++)
+				{
+					limitesRecicladoras[nivelAtual-1,i] = 0;
+					int valor = 0;
+					if (valores.Length > i && int.TryParse(valores[i], out valor))
+					{
+						limitesRecicladoras[nivelAtual-1,i] = valor;
+					}
+				}
+
+			}
+			else if (linhaAtual[0].StartsWith(parseLixoMinimo))
+			{
+				int i = 0;
+				lixosMinimos[nivelAtual-1] = 0;
+				if (int.TryParse(linhaAtual[1], out i))
+				{
+					lixosMinimos[nivelAtual-1] = i;
+				}
 			}
 			else if (linhaAtual[0].StartsWith(parseVelocidadeReciclagem))
 			{
-				float f = 0;
-				velocidades[nivelAtual-1] = 0;
-				if (float.TryParse(linhaAtual[1], out f))
+				linhaAtual[1] = linhaAtual[1].Replace(" ","");
+				linhaAtual[1] = linhaAtual[1].Replace("\t","");
+				
+				string [] valores = linhaAtual[1].Split(',');
+				
+				for (int i = 0; i < quantidadeTiposRecicladoras; i++)
 				{
-					velocidades[nivelAtual-1] = f / 100f;
+					velocidades[nivelAtual-1,i] = 0;
+					float valor = 0;
+					if (valores.Length > i && float.TryParse(valores[i], out valor))
+					{
+						velocidades[nivelAtual-1,i] = valor / 100f;
+					}
 				}
 			}
 			else if (linhaAtual[0].StartsWith(parseDinheiroGerado))
@@ -196,37 +311,60 @@ public class GerenciadorCarregamento : MonoBehaviour
 				linhaAtual[1] = linhaAtual[1].Replace("\t","");
 
 				string [] construcoes = linhaAtual[1].Split(';');
-				Debug.Log ("Linha: "+construcoes[0]+"; "+construcoes[1]);
 
 				if (!construidos.ContainsKey(nivelAtual))
 				{
 					construidos.Add(nivelAtual, new Dictionary<string, int>());
 				}
 
-				foreach(string c in construcoes)
+				if ((construcoes != null && construcoes.Length > 0))
 				{
-					string [] constr = c.Split(':');
-
-					construidos[nivelAtual].Add(constr[0], 1);
-					int i = 0;
-					if (int.TryParse(constr[1], out i))
+					foreach(string c in construcoes)
 					{
-						construidos[nivelAtual][constr[0]] = i;
+						string [] constr = c.Split(':');
+
+						construidos[nivelAtual].Add(constr[0], 1);
+						int i = 0;
+						if (int.TryParse(constr[1], out i))
+						{
+							construidos[nivelAtual][constr[0]] = i;
+						}
 					}
 				}
 
-				Debug.Log ("Nivel "+nivelAtual+": "+construidos[nivelAtual].Count);
+				//Debug.Log ("Nivel "+nivelAtual+": "+construidos[nivelAtual].Count);
 			}
 		}
 
+		// Cria uma string de saída, para debug.
+		/*
 		string saida = "";
-
 		saida += identificador + ":\nnome: " + nome + "\ndescricao: ";
 		saida += descricao + "\nnivel maximo: " + nivelMaximo + "\n";
 		for (int i = 0; i < nivelMaximo; i++)
 		{
 			saida += "nivel " + (i+1) + "\n";
-			saida += "velocidade: " + velocidades[i] + "\n";
+			saida += "velocidades:";
+			for (int j = 0; j < quantidadeTiposRecicladoras; j++)
+			{
+				saida += " "+ velocidades[i,j]+";";
+			}
+			saida += "\n";
+			saida += "valores venda:";
+			for (int j = 0; j < quantidadeTiposRecicladoras; j++)
+			{
+				saida += " "+ valoresVenda[i,j]+";";
+			}
+			saida += "\n";
+			saida += "limite filas:";
+			for (int j = 0; j < quantidadeTiposRecicladoras; j++)
+			{
+				saida += " "+ limitesRecicladoras[i,j]+";";
+			}
+			saida += "\n";
+			saida += "experiencia: "+experiencias[i] + "\n";
+			saida += "velocidade lixo: "+velocidadesLixos[i] + "\n";
+			saida += "lixo minimo: "+lixosMinimos[i] + "\n";
 			saida += "dinheiro: " + dinheiros[i] + "\n";
 			saida += "automatico: " + separacoes[i] + "\n";
 			saida += "poder: " + poderes[i] + "\n";
@@ -239,10 +377,89 @@ public class GerenciadorCarregamento : MonoBehaviour
 			}
 			saida += "\n";
 		}
-
 		Debug.Log (saida);
+		//*/
 
-		return null;
+		//
+		Empreendimento empreendimento = new Empreendimento();
+		empreendimento.identificador = identificador;
+		empreendimento.nome = nome;
+
+		empreendimento._limiteRecicladoras = new Vector4[nivelMaximo];
+			for (int i = 0; i < nivelMaximo; i++)
+			{
+				empreendimento._limiteRecicladoras[i].x = limitesRecicladoras[i, 0];
+				empreendimento._limiteRecicladoras[i].y = limitesRecicladoras[i, 1];
+				empreendimento._limiteRecicladoras[i].z = limitesRecicladoras[i, 2];
+				empreendimento._limiteRecicladoras[i].w = limitesRecicladoras[i, 3];
+			}
+		empreendimento._velocidadeReciclagem = new Vector4[nivelMaximo];
+			for (int i = 0; i < nivelMaximo; i++)
+			{
+				empreendimento._velocidadeReciclagem[i].x = velocidades[i, 0];
+				empreendimento._velocidadeReciclagem[i].y = velocidades[i, 1];
+				empreendimento._velocidadeReciclagem[i].z = velocidades[i, 2];
+				empreendimento._velocidadeReciclagem[i].w = velocidades[i, 3];
+			}
+		empreendimento._valorDeVenda = new Vector4[nivelMaximo];
+			for (int i = 0; i < nivelMaximo; i++)
+			{
+				empreendimento._valorDeVenda[i].x = valoresVenda[i, 0];
+				empreendimento._valorDeVenda[i].y = valoresVenda[i, 1];
+				empreendimento._valorDeVenda[i].z = valoresVenda[i, 2];
+				empreendimento._valorDeVenda[i].w = valoresVenda[i, 3];
+			}
+
+		empreendimento._descricao = new string[nivelMaximo + 1];
+			for (int i = 0; i < (nivelMaximo + 1); i++)
+			{
+				empreendimento._descricao[i] = descricao;
+			}
+		empreendimento._aumentoXP = new float[nivelMaximo];
+			for (int i = 0; i < nivelMaximo; i++)
+			{
+				empreendimento._aumentoXP[i] = experiencias[i];
+			}
+		empreendimento._velocidadeAparecerLixo = new float[nivelMaximo];
+			for (int i = 0; i < nivelMaximo; i++)
+			{
+				empreendimento._velocidadeAparecerLixo[i] = velocidadesLixos[i];
+			}
+		empreendimento._nivelMinimoLixo = new int[nivelMaximo];
+			for (int i = 0; i < nivelMaximo; i++)
+			{
+				empreendimento._nivelMinimoLixo[i] = lixosMinimos[i];
+			}
+		empreendimento._dinheiroPorTempo = new long[nivelMaximo];
+			for (int i = 0; i < nivelMaximo; i++)
+			{
+				empreendimento._dinheiroPorTempo[i] = (long) dinheiros[i];
+			}
+		empreendimento._separacaoAutomatica = new int[nivelMaximo];
+			for (int i = 0; i < nivelMaximo; i++)
+			{
+				empreendimento._separacaoAutomatica[i] = separacoes[i];
+			}
+		empreendimento._taxaSeparacaoLixo = new int[nivelMaximo];
+			for (int i = 0; i < nivelMaximo; i++)
+			{
+				empreendimento._taxaSeparacaoLixo[i] = poderes[i];
+			}
+
+		empreendimento._custos = new long[nivelMaximo];
+			for (int i = 0; i < nivelMaximo; i++)
+			{
+				empreendimento._custos[i] = (long) precos[i];
+			}
+		empreendimento._nivelRequisito = new int[nivelMaximo];
+			for (int i = 0; i < nivelMaximo; i++)
+			{
+				empreendimento._nivelRequisito[i] = sustentabilidades[i];
+			}
+		empreendimento._empreendimentosRequisitos = construidos;
+
+
+		return empreendimento;
 	}
 
 	/// <summary>
