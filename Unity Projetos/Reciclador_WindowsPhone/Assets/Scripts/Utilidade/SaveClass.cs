184eade2afe2d644866d7aad0320d8ceb6390658
Jeferson Barrile Tomazella <jefbt@outlook.com>
2017-01-15 11:17:41 -0200

    Versão 1.5.1

---
+++ Unity Projetos/Reciclador_WindowsPhone/Assets/Scripts/Utilidade/SaveClass.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveClass: MonoBehaviour
+{
+	public void Save()
+	{
+		Jogador.SalvarTudo();
+	}
+
+	public void Load()
+	{
+		Jogador.CarregarTudo();
+	}
+}
+
